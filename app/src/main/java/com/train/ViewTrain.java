4136aa1d75ac33d463997e350ee57c0a7a453db2
Nashitha Lakmal <nashithalakmal@gmail.com>
2019-08-05 16:17:06 +0530

    upadate viewTrain

--- app/src/main/java/com/train/ViewTrain.java
+++ app/src/main/java/com/train/ViewTrain.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,6 +25,7 @@ public class ViewTrain extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.view_train, container, false);
         // Inflate the layout for this fragment
+        tView = (ListView)view.findViewById(R.id.trainView);
         return view;
     }
 
@@ -32,14 +34,20 @@ public class ViewTrain extends Fragment {
         ((MainActivity) getActivity())
                 .setActionBarTitle("ViewTrain");
 
-        tView = (ListView)view.findViewById(R.id.trainView);
+
         ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this.getActivity(),
                 android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.viewTrain));
 
         tView.setAdapter(listAdapter);
 
-        
+        tView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //add a new fragment
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, new Train()).addToBackStack(null).commit();
+            }
+        });
+
     }
 
 
