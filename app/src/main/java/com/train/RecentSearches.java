ffee7303c18adaea1a537bb78ce7827b4a617cc7
NipunEranda <namarasekara71@gmail.com>
2019-08-05 19:20:20 +0530

    recentSearchUpdated

--- app/src/main/java/com/train/RecentSearches.java
+++ app/src/main/java/com/train/RecentSearches.java
@@ -11,25 +11,47 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RecentSearches extends Fragment implements AdapterView.OnItemClickListener {
+import java.util.ArrayList;
 
-    ListView recentSearches;
+public class RecentSearches extends Fragment implements View.OnClickListener {
+
+    ListView recentSearchList;
+    ArrayList<String> names = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    Button viewRecentSearches, deleteRecentSearches, clearRecentSearch;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.recent_searches, container, false);
 
-        recentSearches = (ListView)view.findViewById(R.id.userTimeTableList);
+        deleteRecentSearches = (Button) view.findViewById(R.id.recentViewDeleteBtn);
+        recentSearchList = (ListView) view.findViewById(R.id.listView1);
+        viewRecentSearches = (Button) view.findViewById(R.id.recentViewBtn);
+        clearRecentSearch = (Button) view.findViewById(R.id.clearRecentBtn);
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_single_choice, names);
 
-        ArrayAdapter<String> startStationAdapter = new ArrayAdapter<String>(this.getActivity(),
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.recentSearch));
-        recentSearches.setAdapter(startStationAdapter);
-        recentSearches.setOnItemClickListener(this);
+        recentSearchList.setAdapter(adapter);
+        deleteRecentSearches.setOnClickListener(this);
+        viewRecentSearches.setOnClickListener(this);
+        clearRecentSearch.setOnClickListener(this);
+
+        if (adapter.getCount() == 0) {
+            adapter.add("lol1");
+            adapter.add("lol2");
+            adapter.add("lol3");
+            adapter.add("lol4");
+            adapter.add("lol5");
+            adapter.add("lol6");
+        }else{
+
+        }
 
         return view;
     }
@@ -42,9 +64,44 @@ public class RecentSearches extends Fragment implements AdapterView.OnItemClickL
     }
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        TextView tv = (TextView)view;
-        Toast.makeText(getContext(), tv.getText(), Toast.LENGTH_SHORT).show();
-        getFragmentManager().beginTransaction().replace(R.id.fragment_container, new EditTimeTable()).addToBackStack(null).commit();
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.recentViewBtn:
+                view();
+                break;
+
+            case R.id.recentViewDeleteBtn:
+                delete();
+                break;
+
+            case R.id.clearRecentBtn:
+                clear();
+                break;
+        }
+    }
+
+    private void delete() {
+        int pos = recentSearchList.getCheckedItemPosition();
+
+        if (pos < 0 || adapter.getCount() <= 0) {
+
+        } else {
+            adapter.remove(names.get(pos));
+            adapter.notifyDataSetChanged();
+            Toast.makeText(getContext(), "Deleted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void clear() {
+        adapter.clear();
+    }
+
+    private void view() {
+
+        if (recentSearchList.getCheckedItemPosition() < 0 || adapter.getCount() <= 0) {
+        } else {
+            getFragmentManager().beginTransaction().replace(R.id.fragment_container, new EditTimeTable()).addToBackStack(null).commit();
+        }
     }
 }
