20bfc4350f6b9201e7a27cf2fc54be7fd18b137a
Nashitha Lakmal <nashithalakmal@gmail.com>
2019-08-05 22:02:51 +0530

    more changes

--- app/src/main/java/com/train/AddTrain.java
+++ app/src/main/java/com/train/AddTrain.java
@@ -1,6 +1,8 @@
 package com.train;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -27,6 +29,10 @@ public class AddTrain extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.add_train, container, false);
 
         spinnerType = view.findViewById(R.id.spinner1);
+        savBtn = view.findViewById(R.id.saveBtn);
+        canlBtn = view.findViewById(R.id.cancelBtn);
+        savBtn.setOnClickListener(this);
+        canlBtn.setOnClickListener(this);
 
         return view;
     }
@@ -42,12 +48,50 @@ public class AddTrain extends Fragment implements View.OnClickListener {
         mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerType.setAdapter(mAdapter);
 
-
     }
 
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.saveBtn:
+                AlertDialog.Builder savBuilder = new AlertDialog.Builder(this.getActivity());
+                savBuilder.setMessage("Are you sure, you want to save?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                getFragmentManager().beginTransaction().replace(R.id.fragment_container, new Train()).addToBackStack(null).commit();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                AlertDialog alert = savBuilder.create();
+                alert.show();
+                break;
+            case R.id.cancelBtn:
+                AlertDialog.Builder canBuilder = new AlertDialog.Builder(this.getActivity());
+                canBuilder.setMessage("Are you Sure?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                getFragmentManager().beginTransaction().replace(R.id.fragment_container, new Train()).addToBackStack(null).commit();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                AlertDialog alert1 = canBuilder.create();
+                alert1.show();
+        }
 
     }
 }
